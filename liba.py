581db8a2137bac369d4fda06e9f471a88b3086c4
Sergey Zelinsky <sergzels@gmail.com>
2022-02-09 19:35:02 +0200

    my first commit

---
+++ liba.py
@@ -0,0 +1,4 @@
+a=1111
+def fun(x):
+    return x+1
+    
---
+++ mypy.py
@@ -0,0 +1,14 @@
+from xml.etree.ElementInclude import include
+
+
+import liba as li
+
+for i in range (10):
+
+    print(li.a)
+    print(li.fun(i))
+
+print("Ok")
+
+
+
